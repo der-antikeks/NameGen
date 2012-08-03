4c0acf588b652a8fe55a9999c05800ad737f6bfd
Martin Hildebrandt <der.antikeks@googlemail.com>
2012-08-03 16:00:57 +0200

    changed ssyll vowels from constant to variable
    should help to support a few more languages

--- examples/ssylldivider.go
+++ examples/ssylldivider.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	words := ssyll.DivideText("bAcdEfghI, AEhjklO, vwEgkldcIkd. jAcOb, mAsOn, wIllIAm, jAydEn, nOAh, mIchAEl, EthAn, AlExAndEr, IsAAc, gAvIn, brAydEn, tYlEr. ErgOnOmIc, mOUntAInOUs, vIllAInOUs, bAchElOr, gEnUInE, mAstOdOn, sqUIrmEd.")
+	vowels := "aeiou"
+	words := ssyll.DivideText("bAcdEfghI, AEhjklO, vwEgkldcIkd. jAcOb, mAsOn, wIllIAm, jAydEn, nOAh, mIchAEl, EthAn, AlExAndEr, IsAAc, gAvIn, brAydEn, tYlEr. ErgOnOmIc, mOUntAInOUs, vIllAInOUs, bAchElOr, gEnUInE, mAstOdOn, sqUIrmEd.", vowels)
 	for _, syllables := range words {
 		fmt.Printf("%v => %v\n", strings.Join(syllables, ""), strings.Join(syllables, "|"))
 	}
--- ssyll/divider.go
+++ ssyll/divider.go
@@ -9,25 +9,22 @@ import (
 	"regexp"
 )
 
-// Simple english vowels, ignore Y as only-sometimes-vowel
-const Vowels = "aeiou"
-
 // Divide splits a word into its vowels
-func Divide(word string) (syllables []string) {
+func Divide(word string, vowels string) (syllables []string) {
 	word = strings.ToLower(word)
 	var lv, rv, dist int
 	
 	for len(word) > 0 {
-		lv = strings.IndexAny(word, Vowels)
+		lv = strings.IndexAny(word, vowels)
 		if lv == -1 {
 			syllables = append(syllables, word)
 			break
 		}
 
-		rv = strings.IndexAny(word[lv+1:], Vowels)
+		rv = strings.IndexAny(word[lv+1:], vowels)
 		for rv == 0 {
 			lv++
-			rv = strings.IndexAny(word[lv+1:], Vowels)
+			rv = strings.IndexAny(word[lv+1:], vowels)
 		}
 
 		if rv == -1 {
@@ -46,13 +43,13 @@ func Divide(word string) (syllables []string) {
 }
 
 // DivideText splits a full text into separate words and those into its vowels
-func DivideText(text string) [][]string {
+func DivideText(text string, vowels string) [][]string {
 	ret := [][]string{}
 	text = regexp.MustCompile("[^a-zA-Z]+").ReplaceAllString(text, " ")
 
 	words := strings.Split(text, " ")
 	for _, word := range words {
-		ret = append(ret, Divide(word))
+		ret = append(ret, Divide(word, vowels))
 	}
 
 	return ret
